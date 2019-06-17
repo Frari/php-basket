6049b35f7a49996eddcc8bb1a890344e814c21a3
Frari <francescoriccardi86@gmail.com>
2019-06-17 15:37:36 +0200

    inizio

---
+++ index.php
@@ -0,0 +1,14 @@
+<!-- Ricreare il nostro database dell’esercizio Basket (utilizzando solo funzioni PHP). Questa volta lo creiamo in un file PHP incluso nel file PHP principale che utilizzerà i dati per stamparli nell’html.
+Potete inizialmente stamparli come le card uno sotto (o a fianco) all'altro e poi, se avete tempo e voglia, potete aggiungere il click sul codice del giocatore che mostra i dati solo del giocatore selezionato (con jQuery ovviamente) -->
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+  </body>
+</html>
